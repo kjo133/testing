0a66b04a89b7ebeace236da2068defa99b1afedb
Kevin Olson <kevino93@vt.edu>
2018-03-07 21:21:15 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Random Effects Testing</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+	<div class="container" style="background-color: #c8daf7">
+		<h1>SVG Testing</h1>
+		<svg id="svg" width="580" height="400" xmlns="http://www.w3.org/2000/svg">
+		  <title>Layer 1</title>
+		  <rect id="rect" height="204" width="283" y="111" x="141" stroke-width="1.5" stroke="#000" fill="#a5f7f7"/>
+		 </g>
+		</svg>
+	</div>
+</body>
+</html>
